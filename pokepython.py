480f384c25096357cec37c9b333ad2bcbe266df2
Lucas da Hora Ramos <66744211+Lucas-R4mos@users.noreply.github.com>
2020-07-04 15:37:24 -0300

    Add files via upload

--- pokepython.py
+++ pokepython.py
@@ -1,28 +1,52 @@
+# passar essas imports todos pro arquivo Player e criar a classe Player com elas inseridas. Class Player vai servir para integrar as diversas informações do pokémon para o usuário.
+
 from pokemons import Pokemon
 from pokemons import import_pokemon
+from atributos import Atributos
+from atributos import import_atributo
+from movesets import MoveSet
+from movesets import import_moveset
 from golpes import Golpe
 from golpes import import_golpe
 
 lista_pokemons = import_pokemon('pokemon.csv')
+lista_atributos = import_atributo('pokemon.csv')
+lista_movesets = import_moveset('movesets.csv')
 lista_golpes = import_golpe('moves.csv')
 
-p1 = lista_pokemons[input('Player 1: Escolha um pokémon: ')]
-p1.level = input('{} lvl: '.format(p1.forme))
-print('')
-p2 = lista_pokemons[input('Player 2: Escolha um pokémon: ')]
-p2.level = input('{} lvl: '.format(p2.forme))
-print('')
-print('{} lvl {} vs {} lvl {}'.format(p1.forme, p1.level, p2.forme, p2.level))
-players = [p1, p2]
+class Player:
+    def __init__(self):
+        self.pokemon = lista_pokemons[self]
+        self.atributo = lista_atributos[self]
+        self.movesets = lista_movesets[self]
+        self.golpes = []
+
+pokemon1 = Player(input('Player 1: Escolha um pokémon: '))
+pokemon1.level = input('{} lvl: '.format(pokemon1.Pokemon.forme))
 print('')
 
-# Como a batalha não oferece o uso de itens, por enquanto, mostrar para o jogador o nome, a vida e os golpes do poké.
-# Mostrar os golpes em forma de lista, um em cada linha após o nome do poké. Uma linha após pedir 
-#def batalha(p1.hp, p2.hp):
-#    while (condição de um dos poké morrer para finalizar a batalha)
-#        for p in players: #aqui ele vai sempre perguntar primeiro pro p1 depois pro p2
-#            print('{} {}: HP: {}'.format(p.nome, p.tipo, p.vida)) 
-#            print('Golpes:')   #aqui eu substituo pela função de escolher golpe
-   
+pokemon2 = Player(input('Player 2: Escolha um pokémon: '))
+pokemon2.level = input('{} lvl: '.format(pokemon2.forme))
+print('')
+
+print('{} lvl {} vs {} lvl {}'.format(pokemon1.forme, pokemon1.level, pokemon2.forme, pokemon2.level))
+players = [pokemon1, pokemon2]
+print('')
+
+atributos1.hp = int(atributos1.hp)
+atributos2.hp = int(atributos2.hp)
+while not atributos1.hp <= 0 or atributos2.hp <= 0:
+    for pokemon in players:
+        if pokemon == pokemon1:
+            atributos = atributos1
+            moveset = moveset1
+        if pokemon == pokemon2:
+            atributos = atributos2
+            moveset = moveset2
+        print('{}: HP: {}'.format(pokemon.forme, atributos.hp)) 
+        print('Golpes:')
+        pokemon.golpe_usado = input('Escolha um golpe: ')  
+        print('')
+
 
 # Dano = (((((2 * level) / 5) + 2) * poder * (ataque / defesa) / 50) + 2) * Modificador
