8130a21a7c7277026d43869261d33e86bd0c4b42
Lucas da Hora Ramos <66744211+Lucas-R4mos@users.noreply.github.com>
2020-07-06 00:09:13 -0300

    Update movesets.py

--- movesets.py
+++ movesets.py
@@ -5,15 +5,17 @@ class MoveSet:
 
 def import_moveset(dataset_ulr):
     file = open(dataset_ulr)
+    # D = {Chave pokemon: Value :{chave lv: value move}}
     movesets = {}
     for moveset in file:
         linha = moveset.split(',')
         if not linha[0].isnumeric:
            continue
-        moveset = MoveSet(linha[2])
-        movesets[linha[2]] = moveset
+        golpes = {}
         for move in linha[3:]:
-            if move == '':
+            golpe = move.split(' , ') # Ex.: L3 - Growl -> golpe[0] = L3 e golpe[1] = Growl, logo golpe[0] = quando aprende e golpe[1] = golpe. eu quero chamar pelo nome do golpe
+            if not move.isalnum:
                 continue
-            moveset.moveset.append(move)
+            golpes[golpe[1]] = golpe[0]
+        movesets[linha[2]] = golpes
     return movesets
