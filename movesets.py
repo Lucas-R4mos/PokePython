609609597c46b4a3235174935045f836e347d298
Lucas da Hora Ramos <66744211+Lucas-R4mos@users.noreply.github.com>
2020-07-07 22:09:16 -0300

    Update movesets.py

--- movesets.py
+++ movesets.py
@@ -2,7 +2,7 @@ class MoveSet:
     def __init__(self, forme, golpes_possiveis):
         self.forme = forme
         self.golpes_possiveis = golpes_possiveis
-
+        
 def isvazio(campo):
     if campo == '':
         return True
@@ -17,17 +17,12 @@ def import_moveset(dataset_ulr):
         linha = poke.split(',')
         if not linha[0].isnumeric:
            continue
-        golpes = []
+        golpes = {}
         for golpe in linha[3:]:
             if isvazio(golpe):
                 continue
             linha_golpe = golpe.split(' - ')
-            golpes.append(
-                {
-                    'move' : linha_golpe[-1],
-                    'aprende' : linha_golpe[0]
-                }
-            )
+            golpes[linha_golpe[-1]] = linha_golpe[0]
         moveset = MoveSet(linha[2], golpes)
         movesets[linha[2]] = moveset
     return movesets
