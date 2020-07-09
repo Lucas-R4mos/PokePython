748811252946efdd83647991828e1692504cdf65
Lucas da Hora Ramos <66744211+Lucas-R4mos@users.noreply.github.com>
2020-07-09 03:49:18 -0300

    Implementação das Exceções na escolha de Pokémons.

--- loader.py
+++ loader.py
@@ -1,3 +1,4 @@
+# Estes imports irão alimentar a classe Loader
 from pokemons import Pokemon
 from pokemons import import_pokemon
 from atributos import Atributos
@@ -9,18 +10,21 @@ lista_pokemons = import_pokemon('pokemon.csv')
 lista_atributos = import_atributo('pokemon.csv')
 lista_movesets = import_moveset('movesets.csv')
 
+# A classe Loader surgiu pela necessidade de relacionar os diversos dados de cada Pokémon, de forma que apenas com o 'forme' do Pokémon estes dados são relacionados. Além disto, esta classe também carregará as variáveis de batalha para o Pokémon (Level e golpes).
 class Loader:
     def __init__(self, forme):
         self.forme = forme
         self.pokemon = lista_pokemons[self.forme]
         self.atributos = lista_atributos[self.forme]
         self.movesets = lista_movesets[self.forme]
+        self.level = []
         self.golpe1 = []
         self.golpe2 = []
         self.golpe3 = []
         self.golpe4 = []
         self.golpe_usado = []
-        
+
+# Este import servirá para alimentar a classe Loader.
 def import_loader(dataset_ulr):
     file = open(dataset_ulr)
     loader = {}
@@ -32,36 +36,66 @@ def import_loader(dataset_ulr):
         loader[linha[3]] = poke
     return loader
 
+# Esta função irá fazer todo a escolha de Pokémon para o usuário.
+def escolher_pokemon(player):
+    from loader import Loader
+    from loader import import_loader
+
+    loader = import_loader('pokemon.csv')
+
+    # Escolha do Pokémon
+    confirma = False
+    while not confirma:
+        try:
+            poke = loader[input('{}: Escolha um Pokémon: '.format(player))]
+        except KeyError:
+            print('Opção inválida.')
+        else:
+            confirma = True
+    
+    # Escolha do Level
+    confirma = False
+    while not confirma:
+        try:
+            poke.level = int(input('{} lvl: '.format(poke.forme)))
+        except ValueError:
+            print('Valor inválido.')
+        else:
+            confirma = True
+    
+    # Escolha dos Golpes
+    from golpes import Golpe
+    from golpes import import_golpe
+    from loader import aprende_level
+    from loader import aprende_start
+    from loader import filtra_level
+    # lista_golpes = import_golpe('moves.csv')
+
+    print('Golpes possíveis para {}'.format(poke.forme))
+    for key in poke.movesets.golpes_possiveis:
+        if aprende_start(poke.movesets.golpes_possiveis[key]):
+            print(key + ' : ' + poke.movesets.golpes_possiveis[key])
+        if aprende_level(poke.movesets.golpes_possiveis[key]):
+            level_golpe = filtra_level(poke.movesets.golpes_possiveis[key])
+            if level_golpe <= poke.level:
+                print(key + ' : ' + poke.movesets.golpes_possiveis[key])
+    
+    player = poke
+    return player
+
+# Daqui pra baixo são filtros de expressões regulares.
 def aprende_level(golpe):
     import re
     
-    check = re.findall('\AL', golpe)
+    check = re.findall(r'\AL', golpe)
     return check
-
 def aprende_start(golpe):
     import re
     
-    check = re.findall('\AStart', golpe)
+    check = re.findall(r'\AStart', golpe)
     return check
-
 def filtra_level(valor):
     import re
     
     sub = int(re.sub('L', '', valor))
     return sub
-
-def escolher_golpes(player):
-    from golpes import Golpe
-    from golpes import import_golpe
-    
-    lista_golpes = import_golpe('moves.csv')
-
-    print('Golpes possíveis para {}'.format(player.forme))
-    for key in player.movesets.golpes_possiveis:
-        if aprende_start(player.movesets.golpes_possiveis[key]):
-            print(key + ' : ' + player.movesets.golpes_possiveis[key])
-        if aprende_level(player.movesets.golpes_possiveis[key]):
-            level_golpe = filtra_level(player.movesets.golpes_possiveis[key])
-            if level_golpe <= player.pokemon.level:
-                print(key + ' : ' + player.movesets.golpes_possiveis[key])
-    return
--- pokepython.py
+++ pokepython.py
@@ -1,23 +1,16 @@
-from loader import Loader
-from loader import import_loader
-from loader import escolher_golpes
+print('Esta versão contém erro após a escolha dos Pokémons.')
 
-loader = import_loader('pokemon.csv')
+from loader import escolher_pokemon
 
-player1 = loader[input('Player 1: Escolha um pokémon: ')]
-player1.pokemon.level = int(input('{} lvl: '.format(player1.pokemon.forme)))
-print('')
-escolher_golpes(player1)
-print('')
+player1 = 'Player 1'
+player2 = 'Player 2'
+players = [player1, player2]
+for player in players:
+    escolher_pokemon(player)
+    print('')
 
-
-player2 = loader[input('Player 2: Escolha um pokémon: ')]
-player2.pokemon.level = int(input('{} lvl: '.format(player2.pokemon.forme)))
-print('')
-escolher_golpes(player2)
-print('')
-
-print('{} lvl {} vs {} lvl {}'.format(player1.pokemon.forme, player1.pokemon.level, player2.pokemon.forme, player2.pokemon.level))
+# Com as exceções de escolha prontas, o próximo passo é fazer a relação dos novos dados com os códigos a seguir, que estão no formato dos dados antigos.
+print('{} lvl {} vs {} lvl {}'.format(player1.forme, player1.level, player2.forme, player2.level))
 players = [player1, player2]
 print('')
 
@@ -25,15 +18,15 @@ player1.atributos.hp = int(player1.atributos.hp)
 player2.atributos.hp = int(player2.atributos.hp)
 while player1.atributos.hp <= 0 or player2.atributos.hp <= 0:
     for player in players:
-        print('{} ({}/{}): HP: {}'.format(player.pokemon.forme, player.pokemon.type1, player.pokemon.type2, player.atributos.hp)) 
+        print('{} ({}/{}): HP: {}'.format(player.forme, player.pokemon.type1, player.pokemon.type2, player.atributos.hp)) 
         print('Golpes:')
         for golpe in player.movesets.golpes_possiveis:
             print(golpe)
         player.golpe_usado = input('Escolha um golpe: ')  
         print('')
     
-    print('{} usou {}.'.format(player1.pokemon.forme, player1.golpe_usado))
-    print('{} usou {}.'.format(player2.pokemon.forme, player2.golpe_usado))
+    print('{} usou {}.'.format(player1.forme, player1.golpe_usado))
+    print('{} usou {}.'.format(player2.forme, player2.golpe_usado))
     print('')
 
 
